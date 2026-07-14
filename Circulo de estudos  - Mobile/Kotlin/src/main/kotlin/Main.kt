ac8b414ef029aa27c849b3c8e6ef744193ceea62
Natan Oliveira <natanvsnatan@gmail.com>
2026-07-13 21:19:39 -0300

    aula kotlin

--- Circulo de estudos  - Mobile/Kotlin/src/main/kotlin/Main.kt
+++ Circulo de estudos  - Mobile/Kotlin/src/main/kotlin/Main.kt
@@ -1,23 +1,8 @@
 fun main(){
- informacoes()
-}
-
-fun informacoes(){
-
-    val nome : String = "Natan"
-    var idade: Int = 22
-    val nota: Float = 7.5f // numero decimal de alta precisão - Tamanha : 64bits (8 Bytes)
-    val altura: Double = 7.5 // número decimal de precisão simples = tamanho : 32bits(4 Bytes)
-    val aprovado: Boolean = true
-    val inicial: Char = 'B'
 
 
+ var mediaaluno: Float = media()
+println("Aluno possui nota média de : $mediaaluno")
 
-    println(nome)
-    println(idade)
-    println(nota)
-    println(altura)
-    println(aprovado)
-    println(inicial)
 
 }
---
+++ Circulo de estudos  - Mobile/Kotlin/src/main/kotlin/media.kt
@@ -0,0 +1,9 @@
+fun media(): Float {
+    print("Nota 1 : ")
+    var nota1: Float = readln().toFloat()
+    print("Nota 2 : ")
+    var nota2: Float = readln().toFloat()
+    val x: Float = (nota1 + nota2) / 2
+
+    return x
+}
