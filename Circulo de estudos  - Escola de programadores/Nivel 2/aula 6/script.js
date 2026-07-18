4ed2e25346b602d7ed54c012dfce7858a447e89c
Natan Oliveira <natanvsnatan@gmail.com>
2026-07-18 18:39:16 -0300

    Projeto Api Mago

--- Circulo de estudos  - Escola de programadores/Nivel 2/aula 6/script.js
+++ Circulo de estudos  - Escola de programadores/Nivel 2/aula 6/script.js
@@ -1,82 +1,88 @@
-const body = document.getElementById("corpoSite");
-const titulo = document.querySelector("#textoH1");
-const botao = document.getElementById("botao");
-const lstMenu = document.getElementById("lstMenu");
-const background = document.getElementById("background");
-let index = 0;
-const track = document.getElementById("track");
-const slides = document.querySelectorAll(".slide");
-const totalSlides = slides.length;
+    const body = document.getElementById("corpoSite");
+    const titulo = document.querySelector("#textoH1");
+    const botao = document.getElementById("botao");
+    const lstMenu = document.getElementById("lstMenu");
+    const background = document.getElementById("background");
+    let index = 0;
 
 
-const botaoTeste = () =>{
-    titulo.innerText = "Olá Mundo!";
-
-    body.classList.toggle("dark-mode");
-}
-
-botao.addEventListener('mouseover', function(){
-    titulo.innerText = "Olá Mundo!";
-
-    console.log(body.style.backgroundColor);
-    body.classList.toggle("dark-mode");
-})
-
-document.getElementById("btnMenu").addEventListener('click', () => {
-    lstMenu.classList.toggle("active");
-});
-
-window.addEventListener('scroll', () =>{
-    requestAnimationFrame(() =>{
-        const scroll = window.scrollY;
-        background.style.transform = 'translateY(${scroll * 0.5}px)';
-    });
-});
-
-function updateCarrossel (){
-    track.style.transform = `translateX(-${index * 300}px)`;
-}
 
 
-document.getElementById("next").addEventListener('click', () =>{
 
-    console.log(index);
-    if (index < totalSlides - 1){
-        index++
-    } else {
-        index = 0;
+    const botaoTeste = () =>{
+        titulo.innerText = "Olá Mundo!";
+
+        body.classList.toggle("dark-mode");
     }
 
-    updateCarrossel();
-});
+    botao.addEventListener('mouseover', function(){
+        titulo.innerText = "Olá Mundo!";
 
-document.getElementById("prev").addEventListener('click', () =>{
+        console.log(body.style.backgroundColor);
+        body.classList.toggle("dark-mode");
+    })
 
-    console.log(index);
-    if (index > 0){
-        index--
-    } else {
-        index = totalSlides - 1;
+    document.getElementById("btnMenu").addEventListener('click', () => {
+        lstMenu.classList.toggle("active");
+    });
+
+    window.addEventListener('scroll', () =>{
+        requestAnimationFrame(() =>{
+            const scroll = window.scrollY;
+            background.style.transform = 'translateY(${scroll * 0.5}px)';
+        });
+    });
+
+
+
+    const track = document.getElementById("track");
+    const slides = document.querySelectorAll(".slide");
+    const totalSlides = slides.length;
+
+    function updateCarrossel (){
+        track.style.transform = `translateX(-${index * 300}px)`;
     }
 
-    updateCarrossel();
-});
 
-document.querySelector('.caixa').addEventListener('click', () => {
-    const timeLine = anime.timeline({
-        easing: 'easeOutExpo',
-        duration: 750
+    document.getElementById("next").addEventListener('click', () =>{
+
+        console.log(index);
+        if (index < totalSlides - 1){
+            index++
+        } else {
+            index = 0;
+        }
+
+        updateCarrossel();
     });
 
-    timeLine.add({
-        targets: '.caixa',
-        backgroundColor: '#1aa105',
-        rotate: '1turn',
-        translateX: 150,
-    }).add({
-        rotate: '1turn',
-        targets: '.caixa',
-        backgroundColor: '#a10505',
-        translateX: 0,
+    document.getElementById("prev").addEventListener('click', () =>{
+
+        console.log(index);
+        if (index > 0){
+            index--
+        } else {
+            index = totalSlides - 1;
+        }
+
+        updateCarrossel();
     });
-});
+
+    document.querySelector('.caixa').addEventListener('click', () => {
+        const timeLine = anime.timeline({
+            easing: 'easeOutExpo',
+            duration: 750
+        });
+
+        timeLine.add({
+            targets: '.caixa',
+            backgroundColor: '#1aa105',
+            rotate: '1turn',
+            translateX: 150,
+        }).add({
+            rotate: '1turn',
+            targets: '.caixa',
+            backgroundColor: '#a10505',
+            translateX: 0,
+        });
+    });
---
+++ Circulo de estudos  - Escola de programadores/Projeto Final- Mago Lvl 2/script.js
@@ -0,0 +1,113 @@
+const track = document.getElementById('track');
+const slides = document.querySelectorAll('.slide'); 
+const totalSlides = slides.length;
+let index = 0;
+
+function updateCarrossel(){
+    track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function avancarSlide() {
+    if (index < totalSlides - 1){
+        index++;
+    } else {
+        index = 0;
+    }
+    updateCarrossel();
+}
+
+function voltarSlide() {
+    if (index > 0) {
+        index--;
+    } else {
+        index = totalSlides - 1;
+    }
+    updateCarrossel();
+}
+
+
+document.getElementById('nextDesktop').addEventListener('click', avancarSlide);
+document.getElementById('prevDesktop').addEventListener('click', voltarSlide);
+document.getElementById('nextMobile').addEventListener('click', avancarSlide);
+document.getElementById('prevMobile').addEventListener('click', voltarSlide);
+
+
+
+/*---Page_Visibility_API---- */
+
+const audiovolte = document.getElementById('audioVolte')
+const audiovoltou = document.getElementById('audioVoltou')
+const tituloOriginal = document.title;
+
+document.addEventListener("visibilitychange",() =>{
+    if (document.hidden) {
+        audiovoltou.pause();
+        audiovoltou.currentTime = 0;
+        audiovolte.play();
+        document.title = 'Volte :c';
+    }else{
+        audiovolte.pause();
+        audiovolte.currentTime = 0;
+        audiovoltou.play();
+        document.title = tituloOriginal;
+    }
+})
+
+
+
+async function buscarPersonagem(id){
+    const url =`https://thesimpsonsapi.com/api/characters/${id}`
+
+    try{
+        const resposta = await fetch(url);
+
+        if(!resposta.ok){
+            throw new Error(`Erro`)
+        }
+
+        const personagem = await resposta.json();
+
+        mostrarPersonagem(personagem);
+    } catch (error) {
+        console.error("Falha ao buscar dados", error)
+    }
+}
+
+function mostrarPersonagem(dados){
+
+  const nomePersonagem = document.getElementById('char-nome');
+  const profissaoPersonagem = document.getElementById('char-trabalho');
+  const frasePersonagem = document.getElementById('char-frase');
+  const fotoPersonagem = document.getElementById('char-foto');
+  const idadePersonagem = document.getElementById('char-idade');
+  const statusPersonagem = document.getElementById('char-status');
+  
+  
+
+  nomePersonagem.textContent = dados.name;
+  profissaoPersonagem.textContent = `occupation: ${dados.occupation}`;
+  idadePersonagem.textContent = `Age: ${dados.age}`;
+  statusPersonagem.textContent = `Status: ${dados.status}`
+
+  if (dados.phrases && dados.phrases.length>0) {
+    const fraseAleatoria = dados.phrases[Math.floor(Math.random() * dados.phrases.length)];
+    frasePersonagem.textContent = `"${fraseAleatoria}"`;
+
+  } else {
+    frasePersonagem.textContent = '"..."';
+  }
+
+  fotoPersonagem.src = `https://cdn.thesimpsonsapi.com/500${dados.portrait_path}`;
+  fotoPersonagem.alt = `Foto de ${dados.name}`
+}
+buscarPersonagem(1);
+
+let num = 0
+document.getElementById('char-foto').addEventListener('click', () => {
+
+    
+    buscarPersonagem(num);
+    num ++
+    
+    
+})
