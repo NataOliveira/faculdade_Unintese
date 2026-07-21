a416a6fd66192a05052e135d79bd7dd7e89f9d37
Natan Oliveira <natanvsnatan@gmail.com>
2026-07-20 22:00:31 -0300

    Update Listas.kt

--- Circulo de estudos  - Mobile/Kotlin/src/main/kotlin/Listas.kt
+++ Circulo de estudos  - Mobile/Kotlin/src/main/kotlin/Listas.kt
@@ -1,4 +1,27 @@
-fun listas() {
-    val listaMutavel: MutableList<String> = mutableListOf("Natan")
-    val listaImutavel: List<String> = listOf("natan")
+//fun listas() {
+//    val listaMutavel: MutableList<String> = mutableListOf("Natan")
+//    val listaImutavel: List<String> = listOf("natan")
+//}
+
+
+
+fun main(){
+
+    println("Digite num: ")
+    val nota = readln()?.toDoubleOrNull() ?: 0.0
+
+    if (nota < 6) {
+        println("jamanta")
+    } else{
+        println("passou")
+    }
+
+
+    val lista: List<String> = listOf("A", "B", "C", "D")
+
+    lista.forEach {(
+            println("letra: $it")
+
+    )}
+
 }
