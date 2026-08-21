64718ed5f97865715f901c785a16826c91e8d5e4
Natan Oliveira <natanvsnatan@gmail.com>
2026-08-21 14:43:37 -0300

    Update exercicio.py

--- Estrutura de dados/exercicio.py
+++ Estrutura de dados/exercicio.py
@@ -1,8 +1,10 @@
 from collections import deque
+import msvcrt
 import time
 
-lista_de_concluidos = []
+lista_historico = []
 fila = deque()
+fila_n2 = deque()
 
 class Chamado:
     def __init__(self,id,nome,problema):
@@ -14,29 +16,35 @@ class Chamado:
 
         fila.append(self)
     
-    
 class Atendimento:
     def __init__(self,id):
         self.id = id
       
     def finalizar_chamado(chamado):
         
-        lista_de_concluidos.append(chamado)
+        lista_historico.append(chamado)
 
     def proximo_chamado():
 
         if fila:
-         
          proximo_chamado = fila.popleft()
          return proximo_chamado
         
         else:
             return None
+
+    def transferir_chamado(chamado):
+    
+        fila_n2.append(chamado)
+            
+    def consultar_chamado_anterior():
+    
+        return lista_historico[-1]
         
-    def chamado_anterior():
-
-        return lista_de_concluidos.pop
-
+    def abrir_chamado_anterior():
+        if lista_historico:
+            return lista_historico.pop()
+        
 chamado1 = Chamado('1','Bruna','PC não liga')
 fila.append(chamado1)
 chamado2 = Chamado('2','Carlos','Teclado não funciona')
@@ -50,45 +58,96 @@ fila.append(chamado5)
 
 chamado_em_atendimento = None
 print("="*50)
+logado = True
+while logado:
 
-while len(fila) >= 1:
+    print("\n" * 20 + f""" 
 
+Fila atual : {len(fila)} chamado(s) em espera
+---------Disponível--------------
 
-    opc = int(input("""
     1 - Próximo chamado
-    2 - Finalizar Chamado
-    3 - Consultar Ultimo Chamado \n"""))
+    2 - Consultar Ultimo Chamado
+    3 - Sair do programa \n""")
 
-
+    opc = int(msvcrt.getch()) 
     match opc:
-    
         case 1:
 
             chamado_em_atendimento = Atendimento.proximo_chamado()
 
             if chamado_em_atendimento == None:
-                continue
+                print("\n" * 5 + "Nenhum chamado pendente")
             else: 
-                print(f"""
+                print("\n" * 20 + f""" 
 Nome:{chamado_em_atendimento.nome}
 Descrição do Problema: {chamado_em_atendimento.problema}""")
-        case 2:
-            if chamado_em_atendimento == None:
-                print('Nenhum chamando sendo atendido no momento')
-                continue
-            else:
-                Atendimento.finalizar_chamado(chamado_em_atendimento)
-                print("Chamado finalizado")
-                
 
-        case 3:
-
-            chamado_em_atendimento = Atendimento.chamado_anterior()
+            while opc == 1:
+                print("""
+---------Em Atendimento-------------
             
-            print(f"""
+1 - Finalizar chamado
+2 - Tansferir chamado
+3 - Informações do chamado \n\n""")
+
+                acao = int(msvcrt.getch())
+                match acao:
+
+                    case 1:
+                        if chamado_em_atendimento == None:
+                            print('Nenhum chamando sendo atendido no momento')
+                            opc = 0
+                            
+                        else:
+                            Atendimento.finalizar_chamado(chamado_em_atendimento)
+                            print("Chamado finalizado")
+                            chamado_em_atendimento = None
+                            opc = 0
+
+                    case 2:
+                        print("Chamado Transferido")
+                        Atendimento.transferir_chamado(chamado_em_atendimento)
+                        chamado_em_atendimento = None
+                        opc = 0
+
+                    case 3:
+
+                        if chamado_em_atendimento is None:
+                            print("Nenhum chamado sendo atendido no momento")
+                        else:
+                            print("\n" * 20 + f"""
+ID : {chamado_em_atendimento.id}
 Nome:{chamado_em_atendimento.nome}
 Descrição do Problema: {chamado_em_atendimento.problema}""")
+                    
+        case 2:
 
+            if lista_historico:
+                chamado_em_atendimento = Atendimento.consultar_chamado_anterior()
+                print("\n" * 20 + f"""
+Nome:{chamado_em_atendimento.nome}
+Descrição do Problema: {chamado_em_atendimento.problema}
+
+1 - Reabir chamado
+2 - Voltar""")
+
+                acao = int(msvcrt.getch())
+
+                match acao:
+
+                    case 1:
+                        chamado_em_atendimento = Atendimento.abrir_chamado_anterior()
+                        fila.appendleft(chamado_em_atendimento)
+                    case 2:
+                        pass
+            else: 
+                print("não há histórico de chamados")
+                time.sleep(1)
+        case 3:
+            logado = False
+            print("Atendimento encerrado")
+        
 
 
 
